803bf20e334756f9230b9eabc690f749136b3f1b
Nico <nicotissoni12@gmail.com>
2022-11-05 19:45:04 -0300

    v5.1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './routes/App';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode.MODE>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode.MODE>,
+       <App />
+    </React.StrictMode.MODE>,
   document.getElementById('root')
 );
